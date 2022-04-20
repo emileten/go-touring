e1578f6f43fc3d851710b146c50aea30f10486fc
Emile Tenezakis <e.tenezakis@gmail.com>
2022-04-20 16:09:17 +0900

    add a few exercises answers

---
+++ fibonacci_closure.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+// fibonacci is a function that returns
+// a function that returns an int.
+func fibonacci() func() int {
+	rank := 0
+	n_minus_2 := 0
+	n_minus_1 := 1
+	return func() int {
+		if rank == 0 || rank == 1{
+			to_return := rank
+			rank ++
+			return to_return
+		} else {
+			fibonacci_number := n_minus_2 + n_minus_1
+			n_minus_2 = n_minus_1
+			n_minus_1 = fibonacci_number
+			return fibonacci_number
+		}
+	}
+}
+
+func main() {
+	f := fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Println(f())
+	}
+}
---
+++ word_count_with_map.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"strings"
+
+	"golang.org/x/tour/wc"
+)
+
+func WordCount(s string) map[string]int {
+	var fields []string = strings.Fields(s)
+	var m map[string]int = make(map[string]int)
+	for _, f := range fields {
+		_, ok := m[f]
+		if ok == true {
+			m[f] = m[f] + 1
+		} else {
+			m[f] = 1
+		}
+	}
+	return m
+}
+
+func main() {
+	wc.Test(WordCount)
+}
